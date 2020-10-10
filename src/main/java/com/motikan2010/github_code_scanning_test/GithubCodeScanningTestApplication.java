28afd828e87d7d6946fd820e48da30ac9d6240d2
motikan2010 <motikan2010@yahoo.co.jp>
2020-10-11 02:39:56 +0900

    first commit

---
+++ src/main/java/com/motikan2010/github_code_scanning_test/GithubCodeScanningTestApplication.java
@@ -0,0 +1,13 @@
+package com.motikan2010.github_code_scanning_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCodeScanningTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCodeScanningTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motikan2010/github_code_scanning_test/GithubCodeScanningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motikan2010.github_code_scanning_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCodeScanningTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
